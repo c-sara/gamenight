b9045cee10c5f9281395788f171fa41b6eb83f8a
sara <sara__cai@hotmail.com>
2021-10-05 16:16:41 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+const Category = require('./models/category.js')
+const Game = require('./models/game.js')
+const Player = require('./models/player.js')
+
+const { Pool } = require('pg')
+const db = new Pool({
+    database: 'gamenight'
+})
+
+app.use(express.static('client'))
+
+// assign it in req.body
+app.use(express.json())
+
+app.get('/test', (req, res) => {
+
+  res.send('testing')
+
+})
+
+app.listen(port, () => {
+  console.log('listening on port ' + port)
+})
