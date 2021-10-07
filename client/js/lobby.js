cd74d677100c1ecf701590a51d17620d431d3dea
sarahandreas <sarah.andreas@gmail.com>
2021-10-08 10:00:40 +1100

    nav to game donegit add .

--- client/js/lobby.js
+++ client/js/lobby.js
@@ -1,5 +1,6 @@
 let lobbyName = document.querySelector('.lobby-name')
-let startBtn = document.querySelector('.lobby-play-btn')
+let readyBtn = document.querySelector('.lobby-play-btn')
+let actionsDiv = document.querySelector('.lobby-actions-div')
 
 // renders "you're in lobby x"
 axios.get('/api/lobby')
@@ -11,6 +12,16 @@ axios.get('/api/lobby')
         console.log(err.message)
     })
 
+let ready = []
+
+function createStartAnc() {
+    let startAnc = document.createElement("a")
+    startAnc.href = "/game"
+    startAnc.textContent = "Play"
+    startAnc.classList.add("light-btn")
+    startAnc.classList.add("start-anc")
+    actionsDiv.appendChild(startAnc)
+}
 
 function renderPlayerList() {
 
@@ -22,27 +33,43 @@ function renderPlayerList() {
             
             lobbyPlayerList.innerHTML = ""
 
+            let checkForStartAnc = document.querySelector('.start-anc')
+            if (ready.every(ready => ready === false) !== true && !checkForStartAnc) {
+                readyBtn.remove()
+                createStartAnc()
+            }
+
+            ready = []
+
             players.forEach(player => {
                 let p = document.createElement("p")
                 
                 if (player.host) {
                     p.dataset.id = player.player_id
-                    startBtn.textContent = "Ready"
-                    startBtn.disabled = false
+                    readyBtn.textContent = "Ready"
+                    readyBtn.disabled = false
                 }
 
                 lobbyPlayerList.appendChild(p)
                 p.textContent = player.display_name
+                ready.push(player.ready)
             });
+
+            console.log(ready)
         })
 }
 
 function handleGameStart(e) {
     let playerId = document.querySelector('[data-id]')
-    axios.patch('/api/players/:id')
+        .dataset.id
+    axios.patch(`/api/players/${playerId}`)
+        .then(res => {
+            readyBtn.remove()
+            createStartAnc()
+        })
 }
 
-startBtn.addEventListener('click', handleGameStart)
+readyBtn.addEventListener('click', handleGameStart)
 
 setInterval(renderPlayerList, 3000)
 
