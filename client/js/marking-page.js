2c6d6eaf84480a0d737fc513f1fff5a904f3f0e2
SeeSkyRobinson <cskyrobinson@gmail.com>
2021-10-08 11:21:07 +1000

    branch test

--- client/js/marking-page.js
+++ client/js/marking-page.js
@@ -2,7 +2,7 @@ let answerDivs = document.querySelectorAll('.marking-page-individual-answer-div'
 
 
 function handleAddPoint(e) {
-       let clicked = e.target
+    let clicked = e.target
     let btnOwner = clicked.dataset.userid
     
     if (clicked.classList.contains('marking-page-answer-btn')) {
@@ -16,16 +16,40 @@ function handleAddPoint(e) {
 }
 
 
-// function renderPlayersScores() {
-//     //this populates each players score span according to their player_id. By asking the servers for player score table
+function renderPlayersScores() {
+    //this populates each players score span according to their player_id. By asking the servers for player score table
+    //call to the api asking what the latest scores are
+    //change the textContent of the appropriate btns
+
+    axios.get('api/marking-page')
+        .then(scores => {
+            var scoreArray = scores.data
+
+            scoreArray.forEach(playerScore => {
+                var playerScoreSpan = document.querySelector(`.${playerScore.player_id}_score`)
+                playerScoreSpan.textContent = playerScore.score
+            })
 
 
 
+            //scores.data is an array of objects
 
-// }
+            //for each object in array use DOM to grab the right span
 
+            //update the buttons
+        })
 
-// setInterval(renderPlayersScores, 1000)
+        // 0: {player_id: 1, score: 0}
+        // 1: {player_id: 3, score: 0}
+        // 2: {player_id: 4, score: 0}
+        // 3: {player_id: 2, score: 4}
+        // 4: {player_id: 5, score: 5}
+        // 5: {player_id: 6, score: 0}
+        
+
+}
+
+setInterval(renderPlayersScores, 1000)
 
 answerDivs.forEach(answerDiv => {
     answerDiv.addEventListener('click', handleAddPoint)
--- routes/player_routes.js
+++ routes/player_routes.js
@@ -15,48 +15,36 @@ router.get('/api/players', (req, res) => {
         })
 })
 
+
+//update to filter by game_id
+router.get('/api/marking-page', (req, res) => {
+    //send the client all the player score info stored in players table
+
+    var sql = `SELECT player_id, score FROM players;`
+    db.query(sql)
+        .then(dbRes => {
+            console.log(dbRes.rows);
+            res.json(dbRes.rows)
+        })
+})
+
+
+//update to filter by game_id
 router.put('/api/marking-page', (req, res) => {
-    //this api call has to be responsible for adding one score into the relevant players record
-
-    //determine whether to plus one or minus one.
-
-    //update the table at the appropriate players score column (by first identifing the player who owns the button's id)
-
     var instruction = req.body.scoreChange
-
     var userId = req.body.btnOwner
 
     if (instruction === 'increase') {
         var sql = `UPDATE players SET score = score + 1 WHERE player_id = $1;`
     } else {
         var sql = `UPDATE players SET score = score - 1 WHERE player_id = $1;`
-
     }
-
-
-
     db.query(sql, [userId])
         .then( () => {
             res.json( {} )
         })
-
 })
 
-// router.delete('/api/marking-page', (req, res) => {
-//     //this api call removes one score from relevant player record
-
-// })
-
-
-
-
-
-
-// router.patch('/api/marking-page', (req, res) => {
-
-
-
-// })
 
 router.patch('/api/players/:id', (req, res) => {
     let playerId = req.params.id
